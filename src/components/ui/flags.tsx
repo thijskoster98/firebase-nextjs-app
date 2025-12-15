0e049a6aa1261f85dc631629f41bbd3dbb36aa68
Thijs Koster <thijskoster98@gmail.com>
2025-12-15 11:12:50 +0000

    stretch the union jack a bit taller, just like the dutch flag.

--- src/components/ui/flags.tsx
+++ src/components/ui/flags.tsx
@@ -3,18 +3,18 @@ import { cn } from '@/lib/utils';
 export const GBFlag = ({ className }: { className?: string }) => (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 60 30" 
+        viewBox="0 0 60 40" 
         className={cn("h-auto", className)}
     >
         <clipPath id="a">
-            <path d="M0 0v30h60V0z"/>
+            <path d="M0 0v40h60V0z"/>
         </clipPath>
-        <rect width="60" height="30" fill="#00247d"/>
+        <rect width="60" height="40" fill="#00247d"/>
         <g clipPath="url(#a)">
-            <path d="M0 0l60 30m0-30L0 30" stroke="#fff" strokeWidth="6"/>
-            <path d="M0 0l60 30m0-30L0 30" stroke="#cf142b" strokeWidth="4"/>
-            <path d="M30 0v30M0 15h60" stroke="#fff" strokeWidth="10"/>
-            <path d="M30 0v30M0 15h60" stroke="#cf142b" strokeWidth="6"/>
+            <path d="M0 0l60 40m0-40L0 40" stroke="#fff" strokeWidth="6"/>
+            <path d="M0 0l60 40m0-40L0 40" stroke="#cf142b" strokeWidth="4"/>
+            <path d="M30 0v40M0 20h60" stroke="#fff" strokeWidth="10"/>
+            <path d="M30 0v40M0 20h60" stroke="#cf142b" strokeWidth="6"/>
         </g>
     </svg>
 );
